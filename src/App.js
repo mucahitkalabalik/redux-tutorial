2df98de764506e2eb68b0fb758b07b46252ecaf0
Mücahit <71331160+mucahitkalabalik@users.noreply.github.com>
2025-03-05 17:07:13 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+// import Home from "./pages/Home";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./redux/counterSlice";
+
+import { getCountries } from "./redux/homeSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  let count = useSelector((state) => state.counter.count);
+  let data = useSelector((state) => state.home.homeData);
+  let status = useSelector((state) => state.home.status);
+  let countries = useSelector((state) => state.home.country);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter App</h1>
+      <button onClick={() => dispatch(decrement())}>Increment</button>
+      count: {count}
+      <button onClick={() => dispatch(increment())}>Decrement</button>
+      <br />
+      home : {data.title} <br />
+      data : {data.description}
+      {/* <Home /> */}
+      <button onClick={() => dispatch(getCountries())}>get Country</button>
+      <br></br>
+      {countries?.map((item, key) => {
+        return (
+          <div key={key}>
+            <span>
+              {" "}
+              <b>{item.name.common} - </b>
+            </span>
+            <span>{item.capital} - </span>
+            <span>{item.region}</span>
+          </div>
+        );
+      })}
+
+      {status}
     </div>
   );
 }
